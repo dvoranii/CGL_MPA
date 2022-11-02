bcdfdf290c3fa23627bf87fb2e51639f518e57ae
Ildi Dvorani <dvoranii@ildidev.com>
2022-11-02 14:56:48 -0400

    updated navbar

--- JS/app.js
+++ JS/app.js
@@ -1,5 +1,22 @@
-let submitBtn = document.querySelector(".submit-btn");
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  let submitBtn = document.querySelector(".submit-btn");
 
-submitBtn.addEventListener("click", (e) => {
-  //   e.preventDefault();
-});
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+      }
+    });
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
