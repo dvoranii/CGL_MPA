43cd034ac50dc95cc11f6267273b12cbaa0819ef
Ildi Dvorani <dvoranii@ildidev.com>
2022-11-03 14:28:19 -0400

    moved styles.css file to root directory

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,23 @@
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
-  let submitBtn = document.querySelector(".submit-btn");
+// const navSlide = () => {
+const burger = document.querySelector(".burger");
+const nav = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".nav__link");
+let submitBtn = document.querySelector(".submit-btn");
 
-  burger.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
+burger.addEventListener("click", () => {
+  nav.classList.toggle("nav-active");
 
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
-      }
-    });
-    // Burger animation
-    burger.classList.toggle("toggle");
+  console.log("clicked");
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+    }
   });
-};
+  // Burger animation
+  burger.classList.toggle("toggle");
+});
+// };
 
-navSlide();
+// navSlide();
